fd7c9e3cac558377cc6415b3cd514fa36fa16fc7
Emily Vinsen <emilyv@flocc.co>
2021-06-03 10:50:38 +0100

    menu fix

--- components/utilities/KataMenu.vue
+++ components/utilities/KataMenu.vue
@@ -2,22 +2,34 @@
   <nav class="kata-menu">
     <ul class="kata-menu-list p-0">
       <li v-for="item in menu" :key="item._key" class="nav-link-wrap mb-0">
-        <n-link
-          v-if="item._ref && link(item._ref)"
-          :to="link(item._ref).path"
-          class="nav-link inline-block"
-          @click.native="clickFn"
-        >
-          {{ link(item._ref).title }}
-        </n-link>
-        <a
-          v-else
-          :href="item.link"
-          class="nav-link inline-block"
-          @click="clickFn"
-        >
-          {{ item.title }}
-        </a>
+        <div v-if="typeof item.link == 'object'">
+          <n-link
+            v-if="item.link._ref && link(item.link._ref)"
+            :to="link(item.link._ref).path"
+            class="nav-link inline-block"
+            @click.native="clickFn"
+          >
+            {{ link(item.link._ref).title }}
+          </n-link>
+        </div>
+        <div v-else>
+          <n-link
+            v-if="item._ref && link(item._ref)"
+            :to="link(item._ref).path"
+            class="nav-link inline-block"
+            @click.native="clickFn"
+          >
+            {{ link(item._ref).title }}
+          </n-link>
+          <a
+            v-else
+            :href="item.link"
+            class="nav-link inline-block"
+            @click="clickFn"
+          >
+            {{ item.title }}
+          </a>
+        </div>
       </li>
     </ul>
   </nav>
