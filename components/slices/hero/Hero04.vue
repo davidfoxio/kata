fd3bf6ed9d1e248837bafed5be9855ca58c4e0e7
AndyC <andy@flocc.co>
2021-06-14 11:56:06 +0100

    hero04

--- components/slices/hero/Hero04.vue
+++ components/slices/hero/Hero04.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="slice hero-5 stack-children">
-    <em>this is a placeholder! to be styled.</em>
-    <KataMedia
-      :media="media"
-      :ratio="16 / 9"
-      :max-width="3000"
-      class="h-screen w-full object-cover"
-    />
+  <div class="slice hero-4">
     <div
-      class="w-r10/12 mx-r1/12 md:w-r6/12 md:mx-r3/12 h-screen flex flex-col justify-center text-center z-1"
+      class="w-r10/12 mx-r1/12 md:w-r6/12 md:mx-r3/12 flex flex-col justify-center text-center z-1"
     >
-      <h1 class="heading-1 text-white mb-medium" v-html="title" />
+      <h1 class="heading-1 mb-slice-half" v-html="title" />
+    </div>
+
+    <div class="w-r10/12 mx-r1/12 text-center z-1 mb-slice">
+      <KataMedia
+        :media="media"
+        :ratio="3 / 1"
+        :max-width="3000"
+        class="w-full"
+      />
     </div>
   </div>
 </template>
