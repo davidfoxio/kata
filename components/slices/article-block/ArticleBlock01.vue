e5b78b3379c3f36ff7ae043912f83ce7e4c4ee00
AndyC <andy@flocc.co>
2021-06-14 14:42:59 +0100

    contents menu and article blok ok.

--- components/slices/article-block/ArticleBlock01.vue
+++ components/slices/article-block/ArticleBlock01.vue
@@ -1,24 +1,35 @@
 <template>
   <section
-    class="slice article-block-1"
+    :id="anchor"
+    class="slice article-block-1 mb-slice"
     :class="{
-      flex: imageAlignment,
-      'flex-row-reverse': imageAlignment == 'left',
+      'md:flex': hasImg,
+      'md:flex-row-reverse': imgLeft,
     }"
   >
-    <div :class="{ 'w-2/3': imageAlignment }">
-      <h3 v-if="title" class="mb-large heading-2 fade-up" v-html="title" />
+    <div
+      :class="{
+        'md:w-2/3': hasImg,
+        'md:mr-large': imgRight,
+        'md:ml-large': imgLeft,
+      }"
+    >
+      <span v-if="showNumbers" class="heading-2">{{ number }}.</span>
+      <h3
+        v-if="title"
+        class="mb-large heading-2 fade-up inline"
+        v-html="title"
+      />
       <SanityEmbedContent v-if="textBody" :blocks="textBody" class="fade-up" />
     </div>
-    <div
-      class="sm:w-r5/12 order-1 sm:order-2 mb-large sm:mb-0 sm:relative image"
-    >
+    <div class="md:w-r1/3">
       <KataImage :image="image" :ratio="510 / 714" :max-width="1000" class="" />
     </div>
   </section>
 </template>
 
 <script>
+import camelCase from 'lodash.camelcase'
 import { title, textBody, image } from '../shared'
 export default {
   mixins: [title, textBody, image],
@@ -26,6 +37,28 @@ export default {
     imageAlignment: {
       type: String,
       default: 'right',
+    },
+    number: {
+      type: Number,
+      default: 0,
+    },
+    showNumbers: {
+      type: Boolean,
+      default: () => false,
+    },
+  },
+  computed: {
+    anchor() {
+      return camelCase(this.title)
+    },
+    hasImg() {
+      return this.image && Object.keys(this.image).length
+    },
+    imgLeft() {
+      return this.imageAlignment == 'left'
+    },
+    imgRight() {
+      return this.imageAlignment == 'right'
     },
   },
 }
---
+++ components/utilities/ContentsMenu.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="py-slice-half mb-slice">
+    <nav class="w-r10/12 mx-r1/12">
+      <h2 v-if="menuTitle" class="mb-medium">{{ menuTitle }}</h2>
+      <ol :class="{ numbered: showNumbers }">
+        <li v-for="item in items" :key="item._key">
+          <a
+            :href="'#' + camelCase(item.title)"
+            @click.prevent="scrollToAnchor(camelCase(item.title))"
+          >
+            {{ item.title }}
+          </a>
+        </li>
+      </ol>
+    </nav>
+  </div>
+</template>
+
+<script>
+import camelCase from 'lodash.camelcase'
+
+export default {
+  props: {
+    menuTitle: {
+      type: String,
+      default: '',
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+    showNumbers: {
+      type: Boolean,
+      default: () => false,
+    },
+  },
+  methods: {
+    camelCase(item) {
+      return camelCase(item)
+    },
+    scrollToAnchor(anchor) {
+      let elem = document.getElementById(anchor)
+      window.scrollBy({
+        top: elem.getBoundingClientRect().top - 150,
+        left: 0,
+        behavior: 'smooth',
+      })
+    },
+  },
+}
+</script>
+
+<style>
+ol {
+  column-width: 25ch;
+  column-gap: 10ch;
+}
+.numbered {
+  list-style: decimal;
+}
+</style>
