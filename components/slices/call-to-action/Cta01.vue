785cc020accf418e5b90543000ae052ea5fa0663
Emily Vinsen <emilyv@flocc.co>
2021-07-13 13:09:10 +0100

    cta spacing

--- components/slices/call-to-action/Cta01.vue
+++ components/slices/call-to-action/Cta01.vue
@@ -3,7 +3,7 @@
     <h2 v-if="title" class="heading-2 fade-up mb-medium" v-html="title" />
     <p
       v-if="text"
-      class="fade-up mb-medium sm:px-r1/12 md:px-r2/12"
+      class="fade-up mb-medium sm:px-r1/12 xl:px-r2/12"
       v-html="text"
     />
     <KataLinks v-if="links" :links="links" class="fade-up" />
