75651fbaa2bbfa541cfebb19d20e7c09340886ac
Emily Vinsen <emilyv@flocc.co>
2021-09-02 09:04:10 +0100

    removed ? from queries - package doesn't like it

--- mixins/global.js
+++ mixins/global.js
@@ -1,7 +1,7 @@
 export default {
   methods: {
     getGlobal(global) {
-      if (global.slices?.length == 1) {
+      if (global.slices && global.slices.length == 1) {
         return global.slices[0]
       }
       // @todo - write the get target slice stuff here? migrate from store.
