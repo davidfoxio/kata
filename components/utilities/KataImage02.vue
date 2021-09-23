1f446fd28f60e7631c414ed883d2fa708fd42700
Emily Vinsen <emilyv@flocc.co>
2021-09-23 09:21:44 +0100

    added kataImage02 which doesn't require a ratio, so has no height

---
+++ components/utilities/KataImage02.vue
@@ -0,0 +1,106 @@
+
+<template>
+  <img
+    v-if="imageIsSet"
+    :srcSet="srcSet"
+    :src="src"
+    :sizes="sizes"
+    :class="{ loaded: loaded }"
+    :width="maxWidth"
+    height="auto"
+    class="kata-image"
+    @load="loaded = true"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: Object,
+      // required: true,
+      default: () => {},
+    },
+    loader: {
+      type: Boolean,
+      default: false,
+    },
+    maxWidth: {
+      type: Number,
+      default: 800,
+    },
+    sizes: {
+      type: String,
+      default: '100vw',
+    },
+  },
+  data() {
+    return {
+      loaded: false,
+    }
+  },
+  computed: {
+    imageIsSet() {
+      return this.image?.asset?._ref
+    },
+    theImage() {
+      // we often get an empty object when no image is defined
+      if (this.imageIsSet) {
+        return this.image
+      } else {
+        return {}
+      }
+    },
+    src() {
+      let calcWidth = this.maxWidth / 4
+
+      return this.$imgUrl(this.theImage).width(calcWidth).quality(80).url()
+    },
+    srcSet() {
+      let srcSet = ''
+
+      for (
+        let width = this.maxWidth;
+        width > 200;
+        width -= this.increment(this.maxWidth)
+      ) {
+        srcSet += `${this.$imgUrl(this.theImage)
+          .width(width)
+          .quality(80)
+          .url()} ${width}w,`
+      }
+
+      srcSet = srcSet.slice(0, -1) //remove the trailing comma
+
+      return srcSet
+    },
+  },
+  methods: {
+    increment(maxWidth) {
+      const fiths = Math.floor(maxWidth / 5)
+      let increment = fiths > 200 ? 200 : fiths
+      return increment
+    },
+    h(val) {
+      return Math.floor(val / this.ratio)
+    },
+    imgMeta(ref) {
+      if (ref) return this.$store.getters['references/getImageMetadata'](ref)
+      return null
+    },
+    // onLoad() {
+    // },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+img.kata-image {
+  transition: opacity 1s ease;
+  opacity: 0;
+  &.loaded,
+  &.isLoaded {
+    opacity: 1;
+  }
+}
+</style>
