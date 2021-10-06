476177a15721a7413d8e7c334ad9f0105a83c25d
Emily Vinsen <emilyv@flocc.co>
2021-10-06 11:50:00 +0100

    text06 breakpoint md not sm, and text01 added links

--- components/slices/text/Text01.vue
+++ components/slices/text/Text01.vue
@@ -2,6 +2,7 @@
   <div class="slice text-1 md:flex mx-r1/12 w-r10/12">
     <div class="mb-large md:mb-0 md:w-1/2 md:pr-r1/12 title">
       <h2 v-if="title" v-kata-html="title" class="heading-2 fade-up" />
+      <KataLinks v-if="links" :links="links" class="mt-large fade-up" />
     </div>
     <div class="md:w-1/2 body">
       <SanityEmbedContent v-if="textBody" :blocks="textBody" class="fade-up" />
@@ -10,8 +11,8 @@
 </template>
 
 <script>
-import { title, textBody } from '../shared'
+import { title, textBody, links } from '../shared'
 export default {
-  mixins: [title, textBody],
+  mixins: [title, textBody, links],
 }
 </script>
--- components/slices/text/Text06.vue
+++ components/slices/text/Text06.vue
@@ -1,11 +1,11 @@
 <template>
   <div
-    class="slice text-6 flex flex-col sm:flex-row relative justify-start mx-r1/12 w-r10/12"
+    class="slice text-6 flex flex-col md:flex-row relative justify-start mx-r1/12 w-r10/12"
   >
-    <div class="sm:w-r3/12 sm:mr-r1/12 title">
+    <div class="md:w-r3/12 md:mr-r1/12 title mb-medium md:mb-0">
       <h2 v-if="title" v-kata-html="title" class="heading-3 fade-up" />
     </div>
-    <div class="sm:w-r8/12 body">
+    <div class="md:w-r8/12 body">
       <SanityEmbedContent v-if="textBody" :blocks="textBody" class="fade-up" />
     </div>
   </div>
