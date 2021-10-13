a2f7f76f7119dd5fa051ffefb587a49e148e0ea1
HristoFlocc <hristo@flocc.co>
2021-10-13 09:29:09 +0100

    Fixed bug where News03 nav arrows will become transparent once clicked of

--- components/slices/news/News03.vue
+++ components/slices/news/News03.vue
@@ -90,6 +90,10 @@ export default {
 
 <style lang="scss">
 .news-3 {
+  .slick-prev,
+  .slick-next {
+    background: white !important;
+  }
   .slick-track {
     display: flex;
 
@@ -100,7 +104,6 @@ export default {
   .carousel-nav {
     position: absolute;
     z-index: 50;
-
     &.prev {
       left: var(--screen-border-size);
     }
