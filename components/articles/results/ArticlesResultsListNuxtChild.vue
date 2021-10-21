f7f836547273dc5d102bd73ec2465b58ece1a9f1
Emily Vinsen <emilyv@flocc.co>
2021-10-21 11:08:35 +0100

    portfolio05 condition, results nuxt child update

--- components/articles/results/ArticlesResultsListNuxtChild.vue
+++ components/articles/results/ArticlesResultsListNuxtChild.vue
@@ -1,17 +1,20 @@
 <template>
-  <div v-if="articles" class="py-slice-half px-5 sm:flex sm:space-x-5 relative">
-    <div class="sm:w-1/2">
+  <div
+    v-if="articles"
+    class="py-slice-half px-5 md:flex md:space-x-5 relative articles-results-list-nuxt-child"
+  >
+    <div class="md:w-1/2 list">
       <transition-group tag="ul" name="fade" mode="out-in">
         <li v-for="item in articles" :key="item._id">
           <slot name="tease" :item="item"></slot>
         </li>
       </transition-group>
     </div>
-    <div class="sm:w-1/2 sm:sticky child">
+    <div class="md:w-1/2 md:sticky child">
       <nuxt-child v-if="$route.params.slug" />
     </div>
   </div>
-  <div v-else class="py-slice-half px-5 sm:flex sm:space-x-5">
+  <div v-else class="py-slice-half px-5 md:flex md:space-x-5">
     <p>No results found.</p>
   </div>
 </template>
