4d186dee2cc5530b476cc598cd0b55ffbb02f820
Emily Vinsen <emilyv@flocc.co>
2021-11-23 09:09:13 +0000

    oops, missed this off feature08

--- components/slices/feature/Feature08.vue
+++ components/slices/feature/Feature08.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="slice feature-8" :class="{ 'w-r10/12 mx-r1/12': isContained }">
-    <div class="sm:flex mb-large">
+    <div
+      class="sm:flex mb-large"
+      :class="{ 'w-r10/12 mx-r1/12': !isContained }"
+    >
       <div class="sm:w-r6/12 mr-large">
         <h2 v-kata-html="title" class="heading-2 fade-up" />
       </div>
