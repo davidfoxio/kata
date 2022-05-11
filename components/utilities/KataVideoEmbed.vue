2177dcdac1862e4f5b26cf36e0137d81829c7e01
Emily Vinsen <emilyv@flocc.co>
2022-05-11 16:45:38 +0100

    safari aspect ratio fix

--- components/utilities/KataVideoEmbed.vue
+++ components/utilities/KataVideoEmbed.vue
@@ -82,6 +82,23 @@ export default {
   width: 100%;
   aspect-ratio: 16 / 9;
 
+  @media not all and (min-resolution: 0.001dpcm) {
+    @supports (-webkit-appearance: none) {
+      position: relative;
+      overflow: hidden;
+      width: 100%;
+      padding-top: 56.25%; /* 16:9 Aspect Ratio (divide 9 by 16 = 0.5625) */
+
+      iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+      }
+    }
+  }
+
   iframe {
     width: 100%;
     height: 100%;
