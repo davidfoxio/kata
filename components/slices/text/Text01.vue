26d2157f1c9b1d56db28f9124723743235bc2a8a
HristoFlocc <hristo@flocc.co>
2022-07-19 10:59:49 +0100

    Added slots to text01

--- components/slices/text/Text01.vue
+++ components/slices/text/Text01.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="slice text-1 md:flex mx-r1/12 w-r10/12">
     <div class="mb-large md:mb-0 md:w-1/2 md:pr-r1/12 title">
+      <slot name="top"></slot>
       <h2 v-if="title" v-kata-html="title" class="heading-2 fade-up" />
       <KataLinks v-if="links" :links="links" class="mt-large fade-up" />
     </div>
     <div class="md:w-1/2 body">
       <SanityEmbedContent v-if="textBody" :blocks="textBody" class="fade-up" />
+      <slot name="bottom"></slot>
     </div>
   </div>
 </template>
