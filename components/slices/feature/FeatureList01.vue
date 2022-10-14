5a8a710b21bc5e09d44c2e8efafa049a183840a1
Dan Ling <dan@flocc.co>
2022-10-14 09:56:18 +0100

    v1.1.945

--- components/slices/feature/FeatureList01.vue
+++ components/slices/feature/FeatureList01.vue
@@ -5,6 +5,7 @@
       v-bind="feature"
       :id="'feature-list-item-' + i"
       :key="feature._key"
+      :ratio="ratio"
       :reversed="i % 2 == 0"
       class="mb-large slice-animation"
     />
@@ -17,6 +18,10 @@ export default {
     features: {
       type: Array,
       required: true,
+    },
+    ratio: {
+      type: Number,
+      default: 685 / 514,
     },
   },
 }
