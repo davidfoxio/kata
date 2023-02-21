251ddb19a0c3bd3d0dd7879bc22b726fc04fb770
Emily Vinsen <emilyv@flocc.co>
2023-02-21 13:36:28 +0000

    updated css slider to v2 version with bug fixes

--- components/utilities/KataCssSlider.vue
+++ components/utilities/KataCssSlider.vue
@@ -3,24 +3,34 @@
     <div
       ref="css-slider"
       class="css-slider"
-      :class="{ 'two-slides': images.length == 2 }"
+      :class="{ 'two-slides': images.length == 2, 'no-crop': noCrop }"
+      :style="'min-height:' + minHeight"
     >
       <div
         v-for="(item, i) in images"
         :key="item._id"
         class="slide-image"
         :class="{
-          active: sliderindex == i + 1,
+          active: sliderIndex == i + 1,
           'last-active': lastActive(i),
         }"
       >
         <KataImage
-          v-if="item"
-          data-not-lazy
+          v-if="item && !noCrop"
+          :lazy="false"
           :image="item"
           :ratio="ratio"
+          :mobile-ratio="mobileRatio"
           :max-width="maxWidth"
-          class="h-full max-h-screen object-cover w-full"
+          class="h-full max-h-screen object-cover object-center w-full"
+          :sizes="sizes"
+        />
+        <KataImage02
+          v-else-if="item && noCrop"
+          :lazy="false"
+          :image="item"
+          :max-width="maxWidth"
+          class="max-h-screen object-contain object-center w-full"
           :sizes="sizes"
         />
       </div>
@@ -39,6 +49,14 @@ export default {
       type: Number,
       default: 4 / 3,
     },
+    mobileRatio: {
+      type: Number,
+      default: null,
+    },
+    noCrop: {
+      type: Boolean,
+      default: false,
+    },
     maxWidth: {
       type: Number,
       default: 2000,
@@ -52,16 +70,34 @@ export default {
     return {
       loading: true,
       animate: false,
-      sliderindex: 1,
+      sliderIndex: 1,
+      minHeight: '300px',
     }
   },
   mounted() {
+    if (process.client) {
+      let height = 0
+      let slider = this.$refs['css-slider']
+      let images = slider.getElementsByTagName('img')
+      if (images) {
+        images.forEach((element) => {
+          let h = element.clientHeight
+          // console.log('css slider height', element.clientHeight)
+          if (h > height) {
+            height = h
+          }
+        })
+        if (height > 0) {
+          this.minHeight = height + 'px'
+        }
+      }
+    }
     setInterval(() => {
       this.animate = true
-      if (this.sliderindex < this.images.length) {
-        this.sliderindex++
+      if (this.sliderIndex < this.images.length) {
+        this.sliderIndex++
       } else {
-        this.sliderindex = 1
+        this.sliderIndex = 1
       }
       setTimeout(() => {
         this.animate = false
@@ -70,11 +106,11 @@ export default {
   },
   methods: {
     lastActive(i) {
-      if (this.sliderindex - 1 == 0) {
+      if (this.sliderIndex - 1 == 0) {
         return i + 1 == this.images.length
       }
 
-      return this.sliderindex - 1 == i + 1
+      return this.sliderIndex - 1 == i + 1
     },
   },
 }
@@ -89,51 +125,43 @@ export default {
     width: 100%;
     height: 100%;
     overflow: hidden;
-    min-height: 50vh;
 
     img {
       max-height: calc(100vh - var(--header-height));
-      object-fit: cover;
-      height: 100%;
-      min-height: 100%;
       object-position: center center;
     }
-    @include lg-up {
-      min-height: 70vh;
+
+    &.no-crop {
+      min-height: 56vw;
       img {
+        @apply w-full h-auto object-contain;
+      }
+    }
+
+    &:not(.no-crop) {
+      min-height: 50vh;
+      img {
+        height: 100%;
         min-height: 100%;
       }
     }
 
-    .slide-image {
-      position: absolute;
-      top: 0;
-      left: 0;
-      transition: opacity 1s ease-in;
-      z-index: 1;
-      width: 100%;
-      height: 100%;
-      opacity: 0;
-      overflow: hidden;
+    @include lg-up {
+      min-height: 70vh;
+    }
 
-      &.last-active {
-        z-index: 3;
-        width: 100%;
-        transition: 0;
-        opacity: 1;
+    .slide-image {
+      @apply z-1 overflow-hidden opacity-0 flex items-center justify-center;
+      transition: opacity 1s ease-in;
+
+      img {
+        @apply absolute inset-0 w-full h-auto;
       }
 
       &.active {
         z-index: 5;
-        width: 100%;
         transition: 0;
         opacity: 1;
-      }
-
-      img {
-        height: 100%;
-        width: 100%;
-        object-fit: cover;
       }
     }
 
