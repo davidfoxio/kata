dd70561c068040797721081fab58ed4e6baecf4c
Emily Vinsen <emilyv@flocc.co>
2023-04-26 11:24:39 +0100

    allow overriding of media description

--- src/partials/media.js
+++ src/partials/media.js
@@ -4,14 +4,16 @@ const camelCase = require('lodash.camelcase')
 
 const media = (name = 'Media', vars = {}) => {
   const hidden = vars?.hidden || false
+  const desc =
+    vars?.description ||
+    'Choose image or video media type. You can add multiple images to create a fading `slideshow`. If using a video, you can add a mobile video by adding a second video to the list below.'
   return {
     name: camelCase(name),
     title: name,
     type: 'array',
     editModal: 'popover',
     hidden: hidden,
-    description:
-      'Choose image or video media type. You can add multiple images to create a fading `slideshow`. If using a video, you can add a mobile video by adding a second video to the list below.',
+    description: desc,
     of: [
       standardImage(),
       // videoSrcset(),
